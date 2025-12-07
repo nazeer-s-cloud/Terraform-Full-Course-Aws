9ba28eb0214d4d701ebce571d125e952b1ddea29
nazeer-s-cloud <nazeer06786@gmail.com>
2025-12-07 23:20:02 +0530

    Cleanup old terraform files

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output bucket names with FOR loop
-output "count_bucket_names" {
-  value = [for name in var.s3_bucket_names : name]
-}
-
-# Output IDs from FOR_EACH created buckets
-output "foreach_bucket_ids" {
-  value = { for k, v in aws_s3_bucket.example_each : k => v.id }
-}
