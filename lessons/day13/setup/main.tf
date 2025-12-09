1f2502e1dfb830324e2c0a690d715335b12d7ceb
nazeer <nazeer06786@gmail.com>
2025-12-09 22:15:22 +0530

    Add AWS resources for shared VPC and subnet

--- lessons/day13/setup/main.tf
+++ lessons/day13/setup/main.tf
@@ -1,1 +1,41 @@
+data "aws_vpc" "shared-network-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["Default VPC"]
+  }
+}
+
+data "aws_subnet" "shared-primary-subnet" {
+  vpc_id = data.aws_vpc.shared-network-vpc.id
+
+  filter {
+    name   = "tag:Name"
+    values = ["shared-primary-subnet"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.shared-network-vpc.id]
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "main" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.shared-primary-subnet.id
+
+  tags = {
+    Name = "day013-instance"
+  }
+}
 
