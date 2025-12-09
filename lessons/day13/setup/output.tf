f5d3abd38691be33b854bfacc11ae0a8cce02717
nazeer <nazeer06786@gmail.com>
2025-12-09 22:16:11 +0530

    update output.tf

--- lessons/day13/setup/output.tf
+++ lessons/day13/setup/output.tf
@@ -1,1 +1,15 @@
+output "aws_vpc" {
+    value = data.aws_vpc.shared-network-vpc.id
+}
+
+output "aws_instance_ami" {
+    value = data.aws_ami.amazon_linux_2.id
+  
+}
+
+output "aws_subnet" {
+    value = data.aws_subnet.shared-primary-subnet.id
+  
+}
+
 
